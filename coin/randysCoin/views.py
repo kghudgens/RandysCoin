940b5fec0d90d4a4e658cc8cc8579a7c23db30fa
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-23 19:08:45 +0900

    created view for cryptos page

--- coin/randysCoin/views.py
+++ coin/randysCoin/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "randysCoin/index.html")
+
+
+def cryptos(request):
+    return render(request, "randysCoin/cryptos.html")
