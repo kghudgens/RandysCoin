01b02f5c9a442a7c7b6084d95ef32a6d7ae34114
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-24 19:33:24 +0900

    began consuming coincap api

--- coin/randysCoin/views.py
+++ coin/randysCoin/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests
+import json
+
+r = requests.get("https://api.coincap.io/v2/assets")
+print(r.json())
+
 
 def index(request):
     return render(request, "randysCoin/index.html")
