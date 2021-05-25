0edd4f3799076f926ced41abf755d6c42eb6dcc8
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-25 21:11:00 +0900

    created view for search template

--- coin/randysCoin/urls.py
+++ coin/randysCoin/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("cryptos/", views.cryptos, name="cryptos"),
+    path("search/", views.search, name="search"),
 ]
