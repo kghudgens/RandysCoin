a0178c2a0cb3039ee0249469301a4a4524c7d44f
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-25 21:04:44 +0900

    Page autorefreshes to get new prices

---
+++ coin/randysCoin/static/randysCoin/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// TODO: auto refresh for the crypto page
+// Reload the current document
+
+function autoReload(){
+    location.reload()
+}
+
+window.setTimeout(autoReload, 25000)
