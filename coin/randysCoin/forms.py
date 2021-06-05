a001a3d3a1784bc1884ee77ff80c4d90ca82a31c
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-05 21:44:15 +0900

    Created contact form

--- coin/randysCoin/forms.py
+++ coin/randysCoin/forms.py
@@ -3,3 +3,10 @@ from django import forms
 
 class SearchForm(forms.Form):
     currency = forms.CharField(max_length=20)
+
+
+class ContactForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    subject = forms.TextInput()
