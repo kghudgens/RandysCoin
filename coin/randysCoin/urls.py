4270f67e3336d0953aa67264ccd8f0abe929248d
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-05 21:37:09 +0900

    url path created for contact page

--- coin/randysCoin/urls.py
+++ coin/randysCoin/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("cryptos/", views.cryptos, name="cryptos"),
     path("search/", views.search, name="search"),
+    path("contact/", views.contact, name="contact"),
 ]
