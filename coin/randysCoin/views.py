7849855948a6c2464546de4bd026171a6adf1e25
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-10 21:23:25 +0900

    cleaned contact form and send email

--- coin/randysCoin/views.py
+++ coin/randysCoin/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 from .forms import SearchForm, ContactForm
+
 
 import requests
 
@@ -46,7 +48,17 @@ def contact(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            # Put in logic for posting to database and sending email if contact form is submitted
+            form.save()
+            full_name = form.cleaned_data["first_name"] + form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["subject"]
+            send_mail(
+                "Randy's Coin Contact",
+                message,
+                email,
+                ["hudgens1073@gmail.com"],
+                fail_silently=False,
+            )
             None
     else:
         form = ContactForm()
