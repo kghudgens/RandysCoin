13418529f30683cc03c5969311c4727ba98b1a0c
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-13 19:07:48 +0900

    Created meta function for contact model

--- coin/randysCoin/models.py
+++ coin/randysCoin/models.py
@@ -6,3 +6,6 @@ class Contact(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     subject = models.TextField()
+
+    def __str__(self):
+        return self.email
